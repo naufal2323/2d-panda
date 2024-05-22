3d72864c769f4e9f616cbb6744f10a9cfa759fda
naufal2323 <rayyaj4@gmail.com>
2024-05-22 14:57:08 +0700

    commit hari ini
    cicil

--- Assets/script/platfrom script/platfromSpawner.cs
+++ Assets/script/platfrom script/platfromSpawner.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class platfromSpawner : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class platfromSpawner : MonoBehaviour {
+
+    public GameObject platformPrefab;
+    public GameObject spikedPlatfromprefab;
+    public GameObject[] moving_Platfroms;
+    public GameObject breakablePlatfrom;
+
+    public float platfrom_Spawn_Timer = 2f;
+    private float current_Platfrom_Spawn_Timer;
+
+    private int platfrom_Spawn_Count;
+
+    public float min_X = -2f, max_X = 2f;
+    void Start() {
+        current_Platfrom_Spawn_Timer = platfrom_Spawn_Timer;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void Update() {
+        SpawnPlatfroms();
+    }
+
+    void SpawnPlatfroms () {
+        current_Platfrom_Spawn_Timer -= Time.deltaTime;
+
+        if(current_Platfrom_Spawn_Timer >= platfrom_Spawn_Timer) {
+
+        }
     }
 }

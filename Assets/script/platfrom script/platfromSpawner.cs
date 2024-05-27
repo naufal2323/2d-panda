affe67ab7b566a0d343a0258891a9674c7222a91
naufal2323 <rayyaj4@gmail.com>
2024-05-27 16:41:19 +0700

    progres
    70%

--- Assets/script/platfrom script/platfromSpawner.cs
+++ Assets/script/platfrom script/platfromSpawner.cs
@@ -2,34 +2,88 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class platfromSpawner : MonoBehaviour {
-
+public class PlatformSpawner : MonoBehaviour
+{
     public GameObject platformPrefab;
-    public GameObject spikedPlatfromprefab;
-    public GameObject[] moving_Platfroms;
-    public GameObject breakablePlatfrom;
+    public GameObject spikedPlatformPrefab;
+    public GameObject[] movingPlatforms;
+    public GameObject breakablePlatform;
 
-    public float platfrom_Spawn_Timer = 2f;
-    private float current_Platfrom_Spawn_Timer;
+    public float platformSpawnTimer = 2f;
+    private float currentPlatformSpawnTimer;
 
-    private int platfrom_Spawn_Count;
+    private int platformSpawnCount;
 
-    public float min_X = -2f, max_X = 2f;
-    void Start() {
-        current_Platfrom_Spawn_Timer = platfrom_Spawn_Timer;
-        
+    public float minX = -2f, maxX = 2f;
+
+    void Start()
+    {
+        currentPlatformSpawnTimer = platformSpawnTimer;
     }
 
-
-    void Update() {
-        SpawnPlatfroms();
+    void Update()
+    {
+        SpawnPlatforms();
     }
 
-    void SpawnPlatfroms () {
-        current_Platfrom_Spawn_Timer -= Time.deltaTime;
+    void SpawnPlatforms()
+    {
+        currentPlatformSpawnTimer -= Time.deltaTime;
 
-        if(current_Platfrom_Spawn_Timer >= platfrom_Spawn_Timer) {
+        if (currentPlatformSpawnTimer <= 0)
+        {
+            platformSpawnCount++;
 
+            Vector3 temp = transform.position;
+            temp.x = Random.Range(minX, maxX);
+
+            GameObject newPlatform = null;
+
+            if (platformSpawnCount < 2)
+            {
+                newPlatform = Instantiate(platformPrefab, temp, Quaternion.identity);
+            }
+            else if (platformSpawnCount == 2)
+            {
+                if (Random.Range(0, 2) > 0)
+                {
+                    newPlatform = Instantiate(platformPrefab, temp, Quaternion.identity);
+                }
+                else
+                {
+                    newPlatform = Instantiate(
+                        movingPlatforms[Random.Range(0, movingPlatforms.Length)],
+                        temp, Quaternion.identity
+                    );
+                }
+            }
+            else if (platformSpawnCount == 3)
+            {
+                if (Random.Range(0, 2) > 0)
+                {
+                    newPlatform = Instantiate(platformPrefab, temp, Quaternion.identity);
+                }
+                else
+                {
+                    newPlatform = Instantiate(spikedPlatformPrefab, temp, Quaternion.identity);
+                }
+            }
+            else if (platformSpawnCount == 4)
+            {
+                if (Random.Range(0, 2) > 0)
+                {
+                    newPlatform = Instantiate(platformPrefab, temp, Quaternion.identity);
+                }
+                else
+                {
+                    newPlatform = Instantiate(breakablePlatform, temp, Quaternion.identity);
+                }
+
+                platformSpawnCount = 0; // Reset the count
+            }
+            if(newPlatform)
+                newPlatform.transform.parent = transform;
+                currentPlatformSpawnTimer = platformSpawnTimer;
         }
     }
 }

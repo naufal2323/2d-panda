6c808df65737aa6aefef42958e43d8070e8d4ccd
miftaa07 <miftakhuldzakirah@gmail.com>
2024-11-07 17:43:24 +0700

    update pop up tutorial

--- Assets/script/setting sound sfx/menu.cs
+++ Assets/script/setting sound sfx/menu.cs
@@ -6,8 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class menu : MonoBehaviour
 {
-    public Sprite[] spriteMute; // 0 = on 1 = off
+    public Sprite[] spriteMute; // 0 = on, 1 = off
     public Button buttonMute;
+    public GameObject tutorialCanvas; // Tambahkan referensi ke Canvas tutorial
+
+    private bool tutorialShown = false; // Flag untuk memastikan tutorial hanya ditampilkan sekali
 
     void Start()
     {
@@ -16,8 +19,34 @@ public class menu : MonoBehaviour
 
     public void ButtonIngame()
     {
+        if (!tutorialShown)
+        {
+            // Tampilkan tutorial terlebih dahulu
+            ShowTutorial();
+            tutorialShown = true; // Set flag agar tutorial hanya ditampilkan sekali
+        }
+        else
+        {
+            // Jika tutorial sudah ditampilkan, lanjutkan ke scene permainan
+            StartGame();
+        }
+    }
+
+    private void ShowTutorial()
+    {
+        tutorialCanvas.SetActive(true);
+    }
+
+    public void CloseTutorial()
+    {
+        tutorialCanvas.SetActive(false);
+        StartGame();
+    }
+
+    private void StartGame()
+    {
         SceneManager.LoadScene(1);
-        if(PanelSwitcher._instance != null)
+        if (PanelSwitcher._instance != null)
         {
             PanelSwitcher._instance.NotifyEnterNewGame();
         }

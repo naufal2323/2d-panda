c0deaa19e9a8b07cca6f4930cfa8b4e49c9e72cb
naufal2323 <rayyaj4@gmail.com>
2024-11-15 19:19:46 +0700

    fix moving platfrom speed dan spawn platfrom set

--- Assets/script/platfrom script/platfrom script.cs
+++ Assets/script/platfrom script/platfrom script.cs
@@ -122,12 +122,12 @@ public class PlatformScript : MonoBehaviour
 
             if (movingPlatformLeft)
             {
-                playerMovement.PlatformMove(-0.5f);
+                playerMovement.PlatformMove(-0.8f);
             }
 
             if (movingPlatformRight)
             {
-                playerMovement.PlatformMove(0.5f);
+                playerMovement.PlatformMove(0.8f);
             }
 
             Indicator playerIndicator = target.gameObject.GetComponent<Indicator>();
